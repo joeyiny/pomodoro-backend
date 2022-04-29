f4c6eb21794647f9990457f1a3986619e337d0ff
Joey Iny <joeyainy@gmail.com>
2022-04-28 18:05:54 -0700

    Broadcast timer to all clients

--- index.js
+++ index.js
@@ -14,14 +14,33 @@ app.get("/", (req, res) => {
   res.send("f");
 });
 
+let timerOn = false;
+let interval = null;
+let secondsOnTimer = 25 * 60;
+let sessionType = "Pomodoro";
+
+let toggleTimer = (socket) => {
+  timerOn = !timerOn;
+  io.emit("timer-toggle", timerOn);
+  timerOn
+    ? (interval = setInterval(() => {
+        console.log(--secondsOnTimer);
+        io.emit("timer-tick", secondsOnTimer);
+      }, 1000))
+    : clearInterval(interval);
+};
+
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("disconnect", () => {
     console.log("outie");
   });
-  socket.on("button-press", () => {
-    console.log("Button pressed");
+  socket.on("toggle-button-press", () => {
+    toggleTimer(socket);
   });
+  socket.on("decrement-button-press", () => {});
+  socket.on("increment-button-press", () => {});
+  socket.on("session-type-switch", () => {});
 });
 
 server.listen(3001, () => {
