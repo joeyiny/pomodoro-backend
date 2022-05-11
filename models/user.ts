ccbc227213a0664adbe3bf69180ceb3d718d091e
Joey Iny <joeyainy@gmail.com>
2022-05-11 13:22:35 -0700

    Update user.ts to require unique emails

--- models/user.ts
+++ models/user.ts
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
   {
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     displayName: {
       type: String,
       required: true,
