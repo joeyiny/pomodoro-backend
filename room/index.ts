5cc1ea14a8a4e4d07ffea635421d6d2c311fb4c8
Joey Iny <joeyainy@gmail.com>
2022-05-12 14:46:28 -0700

    Receive databaseId property for each socket user join

--- room/index.ts
+++ room/index.ts
@@ -1,4 +1,3 @@
-import { Socket } from "socket.io";
 const { v4: uuidV4 } = require("uuid");
 
 enum SessionType {
@@ -10,6 +9,7 @@ enum SessionType {
 type User = {
   userName: string;
   roomCode: string;
+  databaseId: string;
 };
 
 interface Users {
@@ -133,25 +133,28 @@ module.exports = function (io) {
         setSessionType(roomCode, sessionType);
       }
     );
-    socket.on("create-room", (userName: string, cb: (arg0: string) => {}) => {
-      let roomCode = uuidV4();
-      let user = { roomCode, userName };
-      allConnectedUsers[socket.id] = user;
-      rooms[roomCode] = {
-        interval: null,
-        timerOn: false,
-        secondsOnTimer: 25 * 60,
-        sessionType: SessionType.POMODORO,
-        connectedUsers: {},
-      };
-      cb(roomCode);
-    });
-    socket.on("join-room", ({ roomCode, userName }, cb) => {
+    socket.on(
+      "create-room",
+      (userName: string, databaseId: string, cb: (arg0: string) => {}) => {
+        let roomCode = uuidV4();
+        let user = { roomCode, userName, databaseId };
+        allConnectedUsers[socket.id] = user;
+        rooms[roomCode] = {
+          interval: null,
+          timerOn: false,
+          secondsOnTimer: 25 * 60,
+          sessionType: SessionType.POMODORO,
+          connectedUsers: {},
+        };
+        cb(roomCode);
+      }
+    );
+    socket.on("join-room", ({ roomCode, userName, databaseId }, cb) => {
       if (!(roomCode in rooms)) {
         cb(false);
         return;
       }
-      let user = { roomCode, userName };
+      let user = { roomCode, userName, databaseId };
       allConnectedUsers[socket.id] = user;
 
       let room = rooms[roomCode];
@@ -173,148 +176,3 @@ module.exports = function (io) {
     });
   });
 };
-
-// export const roomHandler = (socket: Socket) => {
-//   let reset = (roomCode: string, sessionType: SessionType) => {
-//     let minutesToCountdown: number;
-//     let room = rooms[roomCode];
-//     switch (room.sessionType) {
-//       case SessionType.POMODORO:
-//         minutesToCountdown = 25;
-//         break;
-//       case SessionType.SHORTBREAK:
-//         minutesToCountdown = 5;
-//         break;
-//       case SessionType.LONGBREAK:
-//         minutesToCountdown = 15;
-//         break;
-//     }
-
-//     room.timerOn = false;
-//     room.secondsOnTimer = minutesToCountdown * 60;
-//     socket.to(roomCode).emit("timer-toggle", room.timerOn);
-//     socket.to(roomCode).emit("timer-tick", room.secondsOnTimer);
-//   };
-
-//   let setSessionType = (roomCode: string, sessionType: SessionType) => {
-//     let room = rooms[roomCode];
-//     if (room.timerOn) return;
-//     room.sessionType = sessionType;
-//     reset(roomCode, sessionType);
-//     socket.to(roomCode).emit("set-session-type", sessionType);
-//   };
-
-//   let decrement = (roomCode: string) => {
-//     let room = rooms[roomCode];
-//     if (room.timerOn) return;
-//     if (room.secondsOnTimer >= 60) room.secondsOnTimer -= 60;
-//     else room.secondsOnTimer = 0;
-//     socket.emit("timer-tick", room.secondsOnTimer);
-//   };
-
-//   let increment = (roomCode: string) => {
-//     let room = rooms[roomCode];
-//     if (room.timerOn) return;
-//     room.secondsOnTimer += 60;
-//     socket.emit("timer-tick", room.secondsOnTimer);
-//   };
-
-//   let toggleTimer = (roomCode: string, socket) => {
-//     let room = rooms[roomCode];
-
-//     room.timerOn = !room.timerOn;
-//     socket.to(roomCode).emit("timer-toggle", room.timerOn);
-
-//     if (room.timerOn) {
-//       room.interval = setInterval(() => {
-//         room.secondsOnTimer--;
-//         socket.to(roomCode).emit("timer-tick", room.secondsOnTimer);
-//         if (room.secondsOnTimer <= 0) {
-//           room.timerOn = false;
-//           if (room.sessionType === SessionType.POMODORO) {
-//             socket.to(roomCode).emit("completed-pomo");
-//             setSessionType(roomCode, SessionType.SHORTBREAK);
-//           } else {
-//             setSessionType(roomCode, SessionType.POMODORO);
-//           }
-//           socket.to(roomCode).emit("timer-complete");
-//           socket.to(roomCode).emit("timer-toggle", room.timerOn);
-//           clearInterval(room.interval);
-//           reset(roomCode, room.sessionType);
-//           return;
-//         }
-//       }, 1000);
-//     } else clearInterval(room.interval);
-//   };
-//   socket.on("disconnect", () => {
-//     if (!allConnectedUsers[socket.id]) return;
-//     let roomCode = allConnectedUsers[socket.id].roomCode;
-//     let room = rooms[roomCode];
-//     delete allConnectedUsers[socket.id];
-//     delete rooms[roomCode]["connectedUsers"][socket.id];
-//     socket.to(roomCode).emit("connected-users", room["connectedUsers"]);
-//     socket.to(roomCode).emit("user-disconnected", socket.id);
-//     console.log(`User ${socket.id} has disconnected`);
-//   });
-//   socket.on("toggle-button-press", (roomCode: string) => {
-//     toggleTimer(roomCode, socket);
-//   });
-//   socket.on("decrement-button-press", (roomCode: string) => {
-//     decrement(roomCode);
-//   });
-//   socket.on("increment-button-press", (roomCode: string) => {
-//     increment(roomCode);
-//   });
-//   socket.on("video-ready", (roomCode: string) => {
-//     console.log("video ready: " + socket.id + ". room code: " + roomCode);
-//     socket
-//       .to(roomCode)
-//       .emit("connected-users", rooms[roomCode]["connectedUsers"]);
-//     socket.to(roomCode).emit("new-user-joined-video", socket.id);
-//   });
-//   socket.on(
-//     "session-type-switch",
-//     (roomCode: string, sessionType: SessionType) => {
-//       setSessionType(roomCode, sessionType);
-//     }
-//   );
-//   socket.on("create-room", (userName: string, cb: (arg0: string) => {}) => {
-//     let roomCode = uuidV4();
-//     let user = { roomCode, userName };
-//     allConnectedUsers[socket.id] = user;
-//     rooms[roomCode] = {
-//       interval: null,
-//       timerOn: false,
-//       secondsOnTimer: 25 * 60,
-//       sessionType: SessionType.POMODORO,
-//       connectedUsers: {},
-//     };
-//     cb(roomCode);
-//   });
-//   socket.on("join-room", ({ roomCode, userName }, cb) => {
-//     if (!(roomCode in rooms)) {
-//       cb(false);
-//       return;
-//     }
-
-//     let user = { roomCode, userName };
-//     allConnectedUsers[socket.id] = user;
-
-//     let room = rooms[roomCode];
-//     room["connectedUsers"][socket.id] = user;
-
-//     socket.join(roomCode);
-//     socket.to(roomCode).emit("connected-users", room["connectedUsers"]);
-//     socket.broadcast.to(roomCode).emit("new-user-connected", socket.id);
-//     socket.emit("joined-room", roomCode);
-
-//     socket.to(roomCode).emit("timer-toggle", room.timerOn);
-//     socket.to(roomCode).emit("timer-tick", room.secondsOnTimer);
-//     socket.to(roomCode).emit("set-session-type", room.sessionType);
-//     cb(true);
-//   });
-//   socket.on("check-if-room-exists", (roomCode, cb) => {
-//     if (roomCode in rooms) cb({ roomCode: roomCode, exists: true });
-//     else cb({ roomCode: roomCode, exists: false });
-//   });
-// };
